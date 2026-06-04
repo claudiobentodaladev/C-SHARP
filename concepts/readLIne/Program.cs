389d76ac7fb0a71616a5ee39ce8fe2b0253acea6
claudiobentodaladev <programmer.claudio@gmail.com>
2026-06-04 11:39:04 +0100

    READLINE: concepts

---
+++ concepts/readLIne/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace variable;
+
+public class Program{
+    public static void Main(){
+        try{
+            string name;
+            int age;
+
+            Console.WriteLine("Type your name:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Type your age:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"I am {name} and i am {age} years old");
+        } catch{
+            Console.WriteLine("Incorrect Data type!");
+        }
+    }
+}
