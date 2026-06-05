b0715ae31aeaf436983d4bb54e7aac8e2c2f7af7
claudiobentodaladev <programmer.claudio@gmail.com>
2026-06-05 12:54:26 +0100

    EVEN & ODD NUMBERS: exercise

---
+++ exercises/evenOddNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace evenOddNumbers;
+
+public class Program{
+    public static void Main(){
+        Console.WriteLine("===== Even & Odd numbers =====");
+
+        Console.WriteLine("Input a number:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if(number % 2 == 0){
+            Console.WriteLine($"{number} is Even number");
+        } else {
+            Console.WriteLine($"{number} is Odd number");
+        }
+    }
+}
